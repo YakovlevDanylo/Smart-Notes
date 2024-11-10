ed116b2dbcbfd585341e6605f7df8103c7d2f7b7
YakovlevDanylo <166537891+YakovlevDanylo@users.noreply.github.com>
2024-11-10 14:54:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import json
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QApplication, QWidget, QListWidget, QLabel, QPushButton, QLineEdit, QTextEdit, QHBoxLayout, \
     QVBoxLayout
+
+notes = {
+    "Ласкаво просимо!": {
+        "текст": "Вітаю вас у нашому додатку",
+        "теги": ["Вітання", "Привіт"]
+    },
+    "Домашка": {
+        "текст": "Треба зробити домашку до понеділка",
+        "теги": ["Хімія", "Математика"]
+    }
+}
+
+with open("notes_data.json", "w") as file:
+    json.dump(notes, file)
 
 app = QApplication([])
 
@@ -54,6 +69,16 @@ col2.addLayout(row4)
 layout_notes.addLayout(col1, stretch=2)
 layout_notes.addLayout(col2, stretch=1)
 window.setLayout(layout_notes)
+
+def show_notes():
+    key = list_notes.selectedItems()[0].text()
+    field_text.setText(notes[key]["текст"])
+    list_tags.clear()
+    list_tags.addItems(notes[key]["теги"])
+
+list_notes.itemClicked.connect(show_notes)
+
+list_notes.addItems(notes)
 
 window.show()
 app.exec_()
